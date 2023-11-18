10a385d97acf8c33ad08e61c92fa8fc87ac76617
Liam Thorne <lnthorne@shaw.ca>
2023-11-17 23:34:53 -0800

    Render 3d model

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
+
+import { BrowserRouter } from "react-router-dom";
+import { Navbar, Hero, About, Experience, Tech, Works, Contact } from "./components";
 
 const App: React.FC = () => {
-	return <>3D Portfolio</>;
+	return (
+		<BrowserRouter>
+			<div className="relative z-0 bg-primary">
+				<div
+					className="bg-hero-pattern bg-cover
+		    bg-no-repeat bg-center "
+				>
+					<Navbar />
+					<Hero />
+				</div>
+				<About />
+				<Experience />
+				<Tech />
+				<Works />
+			</div>
+			<div className="relative z-0">
+				<Contact />
+			</div>
+		</BrowserRouter>
+	);
 };
 
 export default App;
