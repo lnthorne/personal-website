45894cedf71067a205fbbe09d5d60763ac204f36
Liam Thorne <lnthorne@shaw.ca>
2023-11-17 23:55:16 -0800

    Add spinner and fix lighting

---
+++ src/components/Loader.tsx
@@ -0,0 +1,12 @@
+import { Html } from "@react-three/drei";
+import React from "react";
+
+const CanvasLoader: React.FC = () => {
+	return (
+		<Html>
+			<div className="canvas-spinner"></div>
+		</Html>
+	);
+};
+
+export default CanvasLoader;
--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -8,12 +8,12 @@ const Computers: React.FC = () => {
 	return (
 		<mesh>
 			<hemisphereLight intensity={1.1} groundColor="black" />
-			<pointLight intensity={1.8} />
+			<pointLight intensity={4} />
 			<spotLight
-				position={[-20, 50, 10]}
+				position={[-20, 50, 1]}
 				angle={0.12}
 				penumbra={1}
-				intensity={30}
+				intensity={3000}
 				castShadow
 				shadow-mapSize={1024}
 			/>
@@ -35,8 +35,7 @@ const ComputerCanvas: React.FC = () => {
 			camera={{ position: [20, 3, 5], fov: 25 }}
 			gl={{ preserveDrawingBuffer: true }}
 		>
-			{/* <Suspense fallback={<CanvasLoader />}> */}
-			<Suspense fallback={<></>}>
+			<Suspense fallback={<CanvasLoader />}>
 				<OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
 				<Computers />
 			</Suspense>
