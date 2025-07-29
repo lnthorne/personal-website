c237101150c32ba2a8410a9e8cfb1009b8166257
Liam Thorne <lnthorne@shaw.ca>
2025-07-29 00:54:52 -0600

    fix: donut colour

--- src/components/spinningDonut/SpinningDonut.tsx
+++ src/components/spinningDonut/SpinningDonut.tsx
@@ -5,7 +5,7 @@ import { DonutWrapper } from "./styles";
 const SpinningDonut: React.FC = () => {
 	return (
 		<DonutWrapper>
-			<Donut color="green" />
+			<Donut color="#00ff00" />
 		</DonutWrapper>
 	);
 };
